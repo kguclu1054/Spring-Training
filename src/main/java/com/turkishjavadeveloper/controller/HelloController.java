ae3f813aa4eb6733f9a128e2aa6cc0ae40d20d0a
Kaan Güçlü <kguclu1054@gmail.com>
2024-11-26 01:43:38 +0300

    Added new message handling endpoints - Implemented @GetMapping for '/message/{message}' to return the message - Implemented @PutMapping for '/message/{message}' to update and return the message - Implemented @GetMapping for '/message2' to return the message using @RequestParam

--- src/main/java/com/turkishjavadeveloper/controller/HelloController.java
+++ src/main/java/com/turkishjavadeveloper/controller/HelloController.java
@@ -2,10 +2,12 @@ package com.turkishjavadeveloper.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,6 +35,21 @@ public class HelloController {
 	@PutMapping(path = "/put")
 	public String put() {
 		return "Data Puting!";
+	}
+	
+	@GetMapping(path = "/message/{message}")
+	public String getMyMessage(@PathVariable String message) {
+		return "Your message is : " + message;
+	}
+	
+	@PutMapping(path = "/message/{message}") 
+	public String updateMyMessage(@PathVariable String message) {  
+		return "Updated message is: " + message; 
+		}
+	
+	@GetMapping(path = "/message2")
+	public String getMyMessage_2(@RequestParam String message2) {
+		return "Your message is :"+ message2;
 	}
 
 }
